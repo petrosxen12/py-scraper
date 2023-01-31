d916cbe42eef3b07ac37361960ae939f31dd0229
Petros Xenofontos <petros.x12@gmail.com>
2023-01-31 19:22:44 +0000

    Scrapes all urls using regex to get everything

--- app/scraper.py
+++ app/scraper.py
@@ -1,30 +1,41 @@
 import argparse
-from urllib.request import urlopen
+import re
 
-from bs4 import BeautifulSoup
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--website', help='The website to be scraped')
 args = parser.parse_args()
 
-# website = "http://olympus.realpython.org/profiles/dionysus"
 website = args.website
 
 try:
-    page = urlopen(website)
+    website_source = requests.get(website)
 except ValueError as e:
     print("Error scraping website")
     exit()
 
 # Find all externally loaded sources i.e. images/scripts/fonts not hosted on *website* to be scraped
-# These are any html which include hrefs or src (img and script tags) and start with https:// or www.
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+# These are any html which includes an "=http(s)://" which means loads something (anything) external
 
-externally_loaded_sources = ["img", "script"]
+externally_loaded_regex = r"=\"(http[s]*://([\w*].*))\"(.?)"
 
-for scr in soup.find_all('src'):
-    print(scr)
-    print("------")
-    print("------")
-    print("------")
+ext_loaded_content = re.compile(externally_loaded_regex)
+find_urls = r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
+find_urls_compiled = re.compile(find_urls)
+# print(website_source.text)
+
+content = ext_loaded_content.findall(website_source.text)
+
+external_content = set()
+
+
+for cont in content:
+    match_url = find_urls_compiled.match(cont[0])
+    # print(match_url.group())
+    c = match_url.group()
+
+    external_content.add(c)
+
+for i, c in enumerate(external_content):
+    print(f"{i+1} -- {c}")
